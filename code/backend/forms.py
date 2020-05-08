677e1d4b2ce2f1845e8f5bb2e2fb92e63ed4e792
kinley <kinleylam@cuhk.du.cn>
2020-05-08 12:36:14 +0800

    删除code

--- code/backend/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class InputForm(forms.Form):
-    inputFile = forms.FileField(
-        label='Upload file to input folder',
-    )
--- code/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/juzi/dja_file_management/code/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
